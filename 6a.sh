c231129654171358c4d37b65bfca9dc3c5b98304
Mayura <98948030+mayura1234@users.noreply.github.com>
2022-06-02 17:23:47 +0530

    Update 6a.sh

--- 6a.sh
+++ 6a.sh
@@ -8,10 +8,17 @@ for i in $*
 do
 if [ -f $i ]
 then
+if [ `ls ~/mydir | grep $i* | wc -l` -ne 0 ]
+then
 ls $i
 echo "=================================="
 cat $i
 echo "=================================="
+echo " $i exists in mydir"
+continue 
+fi
+ls $i
+cat $i
 cp $i ~/mydir
 else
 echo "No such file exists"
