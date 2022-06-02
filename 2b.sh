719a73e7b3e628f3de5372d797fd2fed95e87896
Mayura <98948030+mayura1234@users.noreply.github.com>
2022-06-02 07:04:42 +0530

    Update 2b.sh

--- 2b.sh
+++ 2b.sh
@@ -4,7 +4,7 @@ grep $1 /etc/passwd >cd
 if [ $? -eq -0 ]
 then
 echo " User Exists "
-#set -- `grep $1 /etc/passwd | tr ":" " " | cut -d " " -f 1,6`
+set -- `grep $1 /etc/passwd | tr ":" " " | cut -d " " -f 1,6`
 p=`cut -d ":" -f 6 cd`
 echo "User Name : $1 "
 echo "Home Directory : $p"
