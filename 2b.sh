058fe2ed7d531b0428a933152c4023c706fab7ae
Mayura <98948030+mayura1234@users.noreply.github.com>
2022-06-02 07:02:03 +0530

    Display user home directory

---
+++ 2b.sh
@@ -0,0 +1,16 @@
+if [ $# -eq 1 ]
+then
+grep $1 /etc/passwd >cd
+if [ $? -eq -0 ]
+then
+echo " User Exists "
+#set -- `grep $1 /etc/passwd | tr ":" " " | cut -d " " -f 1,6`
+p=`cut -d ":" -f 6 cd`
+echo "User Name : $1 "
+echo "Home Directory : $p"
+else
+echo "User Does not exists"
+fi
+else
+echo "Enter a Parameter"
+fi
